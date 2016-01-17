e7525e06cb8f689b19752e5f1e4dce14595f3a7b
Jan Marsch <jama@jama-mac.local>
2016-01-17 22:51:03 +0100

    NBT read fixed

--- index.js
+++ index.js
@@ -1,56 +1,49 @@
 
 var fs = require('fs');
 var zlib = require('zlib');
-var Long = require('long');
-var nbt = require('prismarine-nbt');
+var Int64 = require('node-int64');
 
+var NBT = require('./lib/NBT.js');
 var World = require('./lib/World.js');
-// var Region  = require('./lib/Region.js');
-var Projection = require('./lib/Projection.js');
-var BLOCKS = require('./config/blocks.js');
+var rasterize = require('./lib/Rasterizer.js');
 
 //*****************************************************************************
 
 var WORLD_NAME = 'TestBerlin01';
 
-var ANCHOR_E = 0;
-var ANCHOR_N = 0;
+var ORIGIN_X = 0;
+var ORIGIN_Y = 0;
 
-var options = {
-  FLATLAND: 0,
-  FLOOD: 0,
-  BLOCKS: BLOCKS,
-  MIN_HEIGHT: 0,
-  MAX_HEIGHT: 256
-};
+var SIZE_X = 0;
+var SIZE_Y = 0;
+
+// var options = {
+//   FLATLAND: 0,
+//   FLOOD: 0
+// };
 
 //*****************************************************************************
 
 try {
   fs.mkdirSync('saves');
   fs.mkdirSync('saves/' + WORLD_NAME);
+  fs.mkdirSync('saves/' + WORLD_NAME + '/region');
 } catch(e) {}
 
 //*****************************************************************************
 
+var nbt = NBT.fromGzipFile('sample/level.dat');
+/*
 console.log('Setting World options');
 
-fs.readFile('sample/level.dat', function(error, data) {
-  nbt.parse(data, function(err, data) {
+nbt.tag.value.Data.value.LevelName.value = WORLD_NAME;
+nbt.tag.value.Data.value.LastPlayed.value = new Int64('' + Date.now());
 
-    data.value.Data.value.LevelName.value = WORLD_NAME;
+nbt.toGzipFile('saves/' + WORLD_NAME + '/level.dat');
 
-    var long = Long.fromString('' + Date.now(), true);
-    data.value.Data.value.LastPlayed.value = [long.high, long.low];
+/*
+var world = new World('saves/' + WORLD_NAME);
+var geojson = JSON.parse(fs.readFileSync('tile.json'));
 
-    fs.writeFileSync('saves/' + WORLD_NAME + '/level.dat', zlib.gzipSync(nbt.writeUncompressed(data)));
-
-    var world = new World('saves/' + WORLD_NAME);
-    var proj = new Projection(world, 0, 0, ANCHOR_E, ANCHOR_N);
-
-    // options.MAPTILES = new MapTiles();
-    // options.ELEVATION = new Elevation();
-
-    proj.render(options);
-  });
-});
+rasterize(world, geojson);
+*/
