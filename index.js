b01ff1d4d8d47a135cc8441b01140e85c03634b3
Jan Marsch <jama@jama-mac.local>
2016-01-20 01:25:58 +0100

    NBT static methods WIP

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 var fs = require('fs');
-var zlib = require('zlib');
-var Int64 = require('node-int64');
+var long = require('long');
 
 var NBT = require('./lib/NBT.js');
 var World = require('./lib/World.js');
@@ -26,24 +25,26 @@ var SIZE_Y = 0;
 
 try {
   fs.mkdirSync('saves');
+} catch(e) {}
+
+try {
   fs.mkdirSync('saves/' + WORLD_NAME);
+} catch(e) {}
+
+try {
   fs.mkdirSync('saves/' + WORLD_NAME + '/region');
 } catch(e) {}
 
 //*****************************************************************************
 
-var nbt = NBT.fromGzipFile('sample/level.dat');
-/*
 console.log('Setting World options');
 
+var nbt = NBT.fromGzipFile('sample/level.dat');
 nbt.tag.value.Data.value.LevelName.value = WORLD_NAME;
-nbt.tag.value.Data.value.LastPlayed.value = new Int64('' + Date.now());
-
+nbt.tag.value.Data.value.LastPlayed.value = long.fromNumber(Date.now());
 nbt.toGzipFile('saves/' + WORLD_NAME + '/level.dat');
 
-/*
 var world = new World('saves/' + WORLD_NAME);
 var geojson = JSON.parse(fs.readFileSync('tile.json'));
 
 rasterize(world, geojson);
-*/
