df65017fe5401b4cf1b4e35b6ff25c008305808f
Jan Marsch <jama@jama-mac.local>
2016-01-24 03:31:17 +0100

    NBT reorg

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 var fs = require('fs');
-var long = require('long');
 
 var NBT = require('./lib/NBT.js');
 var World = require('./lib/World.js');
@@ -28,12 +27,14 @@ try {
 
 console.log('Setting World options');
 
-var nbt = NBT.fromGzipFile('sample/level.dat');
-nbt.tag.value.Data.value.LevelName.value = WORLD_NAME;
-nbt.tag.value.Data.value.LastPlayed.value = long.fromNumber(Date.now());
-nbt.toGzipFile('saves/' + WORLD_NAME + '/level.dat');
+var level = NBT.fromGzipFile('sample/level.dat');
+level.value.Data.value.LevelName.value = WORLD_NAME;
+level.value.Data.value.LastPlayed.value = Date.now();
+NBT.toGzipFile('saves/' + WORLD_NAME + '/level.dat', level);
 
 var world = new World('saves/' + WORLD_NAME);
 var geojson = JSON.parse(fs.readFileSync('data/tile.json'));
 
 rasterize(world, geojson);
+
+console.log('done');
